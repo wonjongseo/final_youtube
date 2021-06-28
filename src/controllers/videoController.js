b27696fef7af0a4685af21c90caa84fcbd9a9616
wonjongseo <visionwill@naver.com>
2021-06-28 17:18:56 +0900

    update template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,33 @@
-export const trending = (req, res) => res.render("home", {titlePage: "Home"});
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Thrid Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 1,
+        },
+    ];
+
+    return res.render("home", {videos});
+};
 export const see = (req, res) => res.render("watch", {titlePage: "Watch"});
 export const edit = (req, res) => res.render("edit", {titlePage: "Edit"});
 export const search = (req, res) => res.send("Search");
