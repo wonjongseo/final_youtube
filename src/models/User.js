4e5531a5ad84b71701f4d7c1caab90aa5e7093cb
wonjongseo <visionwill@naver.com>
2021-08-26 17:07:37 +0900

    .

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     socialOnly: {type: Boolean, default: false},
     username: {type: String, required: true, unique: true},
     password: {type: String},
-    name: {type: String, required: true},
+    name: {type: String},
     location: String,
     comments: [{type: mongoose.Schema.Types.ObjectId, ref: "Comment"}],
     videos: [{type: mongoose.Schema.Types.ObjectId, ref: "Video"}],
