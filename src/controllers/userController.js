061b7c6f6583b23ed7294868c197f9cb7b526728
wonjongseo <visionwill@naver.com>
2022-02-25 14:49:55 +0900

    fix server

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,16 +3,21 @@ import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
+
 export const postJoin = async (req, res) => {
     const {name, username, email, password, password2, location} = req.body;
+
     const pageTitle = "Join";
+
     if (password !== password2) {
         return res.status(400).render("join", {
             pageTitle,
             errorMessage: "Password confirmation does not match.",
         });
     }
+
     const exists = await User.exists({$or: [{username}, {email}]});
+
     if (exists) {
         return res.status(400).render("join", {
             pageTitle,
