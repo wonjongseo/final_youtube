d6abd908b5c83c5d69471bc0007f13ec667edccd
wonjongseo <visionwill@naver.com>
2022-06-22 00:13:32 +0900

    change title and footer

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,7 +6,7 @@ export const home = async (req, res) => {
     const videos = await Video.find({})
         .sort({createdAt: "desc"})
         .populate("owner");
-    return res.render("home", {pageTitle: "Home", videos});
+    return res.render("home", {pageTitle: "Jongtube", videos});
 };
 
 export const watch = async (req, res) => {
